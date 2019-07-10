8d3584abf73a45be3a5f8eecae7c6a46aa2d1d61
wengyulin <244181866@qq.com>
2019-07-10 17:24:01 +0800

    weng

--- src/pages/Roles/Roles.tsx
+++ src/pages/Roles/Roles.tsx
@@ -1,89 +1,109 @@
 import React from 'react';
-import {Card, Table} from 'antd';
+import {Card, Table, Input, Divider, Row, Col, Button} from 'antd';
+import styless from './Roles.module.scss';
 const Roles = () => {
-    const dataSource = [
-      {
-        id: '0001',
-        Charactername: '系统管理员',
-        tongyong: '是',
-        ApplicationOrganization: '全部',
-        juese: 'aaa',
-        caozuo: '编辑|角色授权|删除',
-      },
-      {
-        id: '0002',
-        Charactername: '气源采购',
-        tongyong: '是',
-        ApplicationOrganization: '全部',
-        juese: 'aaa',
-        caozuo: '编辑|角色授权|删除',
-      },
-      {
-        id: '0003',
-        Charactername: '气源跟踪',
-        tongyong: '是',
-        ApplicationOrganization: '全部',
-        juese: 'aaa',
-        caozuo: '编辑|角色授权|删除',
-      },
-      {
-        id: '0004',
-        Charactername: '结算校核',
-        tongyong: '是',
-        ApplicationOrganization: '全部',
-        juese: 'aaa',
-        caozuo: '编辑|角色授权|删除',
-      },
-    ];
-    const columns = [
-        {
-          title: '角色ID',
-          key: 'id',
-          dataIndex: 'id',
-        },
-        {
-          title: '角色名称',
-          key: 'Charactername',
-          dataIndex: 'Charactername',
-        },
-        {
-          title: '是否通用',
-          key: 'tongyong',
-          dataIndex: 'tongyong',
-        },
-        {
-          title: '应用组织',
-          key: 'ApplicationOrganization',
-          dataIndex: 'ApplicationOrganization',
-        },
-        {
-          title: '角色描述',
-          key: 'juese',
-          dataIndex: 'juese',
-        },
-        {
-          title: '操作',
-          key: 'caozuo',
-          dataIndex: 'caozuo',
-        }
-];
-return (
-  <>
-    <div>
-      <Card title="基础表格">
-        <Table
-          bordered
-          columns={columns}
-          dataSource={dataSource}
-          pagination={false}
-        />
-      </Card>
-    </div>
-  </>
-);
+  const dataSource = [
+    {
+      id: '0001',
+      Charactername: '系统管理员',
+      tongyong: '是',
+      ApplicationOrganization: '全部',
+      juese: 'aaa',
+    },
+    {
+      id: '0002',
+      Charactername: '气源采购',
+      tongyong: '是',
+      ApplicationOrganization: '全部',
+      juese: 'aaa',
+    },
+    {
+      id: '0003',
+      Charactername: '气源跟踪',
+      tongyong: '是',
+      ApplicationOrganization: '全部',
+      juese: 'aaa',
+    },
+    {
+      id: '0004',
+      Charactername: '结算校核',
+      tongyong: '是',
+      ApplicationOrganization: '全部',
+      juese: 'aaa',
+    },
+  ];
+  const columns = [
+    {
+      title: '角色ID',
+      key: 'id',
+      dataIndex: 'id',
+    },
+    {
+      title: '角色名称',
+      key: 'Charactername',
+      dataIndex: 'Charactername',
+    },
+    {
+      title: '是否通用',
+      key: 'tongyong',
+      dataIndex: 'tongyong',
+    },
+    {
+      title: '应用组织',
+      key: 'ApplicationOrganization',
+      dataIndex: 'ApplicationOrganization',
+    },
+    {
+      title: '角色描述',
+      key: 'juese',
+      dataIndex: 'juese',
+    },
+    {
+      title: '操作',
+      key: 'caozuo',
+      dataIndex: '操作',
+      render: (record: any) => (
+        <span>
+          <a href="javascript:;" onClick={() => console.log('我被点击了')}>
+            编辑
+          </a>
+          <Divider type="vertical" />
+          <a href="javascript:;">角色授权</a>
+          <Divider type="vertical" />
+          <a href="javascript:;" style={{color: 'red'}}>
+            删除
+          </a>
+        </span>
+      ),
+    },
+  ];
+  return (
+    <>
+      <div>
+        <Card>
+          <Row gutter={16}>
+            <Col span={4}>
+              <span style={{display: 'inline-block'}}>角色名称/编码：</span>
+              <Input className={styless.contents} />
+            </Col>
+            <Col>
+              <Button type="primary" className={styless.btns}>
+                搜索
+              </Button>
+            </Col>
+          </Row>
 
-}
-
-
+          <Table
+            bordered
+            columns={columns}
+            dataSource={dataSource}
+            pagination={false}
+            size="middle"
+          />
+        </Card>
+      </div>
+    </>
+  );
+};
 
 export {Roles};
